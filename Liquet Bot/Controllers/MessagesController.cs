d5632778de41ab955b1005650f8fb0d498238f51
Aldrin Natividad <aldmnatividad@gmail.com>
2017-09-22 14:45:17 -0400

    Adding Files
    Self Explanatory

---
+++ Liquet Bot/Controllers/MessagesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Liquet_Bot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                //Calculates something for us to return
+                
+                string message = (activity.Text ?? string.Empty);
+                int length = message.Length - 1;
+                if (message[0] == '~')
+                {
+                    //return our reply to the user
+                    Activity reply = activity.CreateReply($"You sent \"{message.Remove(0, 1)}\" which was {length} characters long.");
+                    Activity liquetBotIsHappy = activity.CreateReply("I'm just really glad I work uwu");
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                    await connector.Conversations.ReplyToActivityAsync(liquetBotIsHappy);
+                    //await connector.Conversations.SendToConversationAsync();
+                }
+
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
